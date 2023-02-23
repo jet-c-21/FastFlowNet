2c742834a13f9ea1be01cbeb6482b49d526246ed
Jet (Ta-Wei Chien)$PUFF-X <67193495+jet-c-21@users.noreply.github.com>
2023-02-23 17:00:02 +0800

    up

---
+++ setup_env.sh
@@ -0,0 +1,5 @@
+cd models/correlation_package
+pip install torch===1.2.0 -f https://download.pytorch.org/whl/torch_stable.html
+#pip install spatial-correlation-sampler==0.2.0
+python setup.py build
+python setup.py install
